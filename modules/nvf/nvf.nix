97215e175539426e294ed6e20af01c215666eb5f
byczax <maciej.byczko@gmail.com>
2025-08-20 11:32:23 +0200

    add typst support in nvim

--- modules/nvf/nvf.nix
+++ modules/nvf/nvf.nix
@@ -164,6 +164,22 @@ in {
                 vim.g.vimtex_compiler_method = 'latexmk' 
               '';
             };
+            typst = {
+              package = pkgs.vimUtils.buildVimPlugin {
+                pname = "typst";
+                version = "master";
+                src = pkgs.fetchFromGitHub {
+                  owner = "kaarmu";
+                  repo = "typst.vim";
+                  rev = "master";
+                  sha256="PfrkOn2c2JhqplTYnm8Lz4H3/0zVUSlA7l+FN0okKDY=";
+                  #sha256 = "6ClxRxHQgr/qRSI7jg071MO5G4GTbHtyTt6iS6Wqq90=";
+                };
+              };
+              #setup = ''
+              #  " any vim.g or vim.cmd setup you want for typst
+              #'';
+            };
           };
         };
      };
