13e7e15be913a9e55c267e06199315aec9c6fac4
byczax <maciej.byczko@gmail.com>
2025-12-09 21:00:26 +0100

    add python nix-shell

---
+++ shell-dev/python.shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python3.withPackages (python-pkgs: [
+        python-pkgs.pandas
+        python-pkgs.requests
+        python-pkgs.faker
+        python-pkgs.beautifulsoup4
+        python-pkgs.flask
+      ]))
+    ];
+  }
