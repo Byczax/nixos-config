01e6ed3adeda4c6165722b77d176a1d4c603d66e
byczax <maciej.byczko@gmail.com>
2025-12-14 16:24:22 +0100

    add additional kanshi setting

--- modules/home/kanshi.mod.nix
+++ modules/home/kanshi.mod.nix
@@ -55,6 +55,38 @@ in {
             ];
           };
         }
+        {
+          profile = {
+            name = "home_office2";
+            outputs = [
+              {
+                criteria = "eDP-1";
+                position = "1920,120";
+                mode = "1920x1080@60.05Hz";
+                status = "enable";
+              }
+              {
+                criteria = "DP-8";
+                position = "5760,0";
+                mode = "1920x1080@60.0Hz";
+                transform = "90";
+                status = "enable";
+              }
+              {
+                criteria = "DP-9";
+                position = "0,0";
+                mode = "1920x1200@59.95Hz";
+                status = "enable";
+              }
+              {
+                criteria = "DP-10";
+                position = "3840,120";
+                mode = "1920x1080@60.0Hz";
+                status = "enable";
+              }
+            ];
+          };
+        }
       ];
     };
   };
