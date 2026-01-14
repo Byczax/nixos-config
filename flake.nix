5f99b19ddcb972da974d051afe89c191d12f63bd
byczax <maciej.byczko@gmail.com>
2026-01-14 23:26:45 +0100

    remove unused inputs

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nur = {
+    #   url = "github:nix-community/NUR";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     aw-watcher-hyprland = {
       url = "github:bobvanderlinden/aw-watcher-window-hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,24 +24,24 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-    watt = {
-      url = "github:notashelf/watt";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    agenix-rekey = {
-      url = "github:oddlama/agenix-rekey";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zedless.url = "github:zedless-editor/zed";
-    impermanence.url = "github:nix-community/impermanence";
-    stylix = {
-      url = "github:nix-community/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # watt = {
+    #   url = "github:notashelf/watt";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # agenix = {
+    #   url = "github:ryantm/agenix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # agenix-rekey = {
+    #   url = "github:oddlama/agenix-rekey";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # zedless.url = "github:zedless-editor/zed";
+    # impermanence.url = "github:nix-community/impermanence";
+    # stylix = {
+    #   url = "github:nix-community/stylix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = inputs: let
